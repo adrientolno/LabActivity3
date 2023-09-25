69879e8cedc188eb504e9cfefca9f9bdeead58e9
Adrien Tolno <tus29878@temple.edu>
2023-09-25 16:55:11 -0400

    Declare the name in the main activity display the name
    created new branch called feature-1
    the user will be prompt to enter the name to be print along with the Hello message, but the user didn't enter the name click the button, it will show an error

--- app/src/main/java/edu/temple/helloworld/MainActivity.kt
+++ app/src/main/java/edu/temple/helloworld/MainActivity.kt
@@ -2,6 +2,7 @@ package edu.temple.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Message
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -10,19 +11,36 @@ class MainActivity : AppCompatActivity() {
 
     // Declare view properties - the first one is done for you
     lateinit var displayTextView: TextView
-
+    lateinit var nameEditText: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         // Initialize with views defined in Layout - the first one is done for you
         displayTextView = findViewById(R.id.displayTextView)
-
+        nameEditText = findViewById(R.id.nameEditText)
         
         findViewById<Button>(R.id.clickMeButton).setOnClickListener {
-            displayTextView.text = "Hello, ${findViewById<EditText>(R.id.nameEditText).text}"
+
+            var hasError = false
+            nameEditText.run {
+                if (text.isBlank()){
+                    hasError = true
+                    showError(this, "Name not entered ")
+                } else {
+
+                    displayTextView.text = "Hello, ${findViewById<EditText>(R.id.nameEditText).text}"
+                }
+            }
+
+
         }
 
+
+    }
+
+    private fun showError(view: TextView, message: CharSequence) {
+        view.error = message
 
     }
 }
